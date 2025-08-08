5695461ac88117aae5cc56b72d7b214802afbf04
Goutham Kumar <gouthamgambir143@gmail.com>
2025-08-08 19:28:18 +0530

    cleaned up old assignment files bin and src directories

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -7,6 +7,87 @@ public class Assignment1_DataTypes {
 		//1.Temperature of a city in degrees Celsius: 25.5
 		float Temperature = 25.5f;
 		System.out.println(Temperature);
-	}
 
+		//	2.Whether a customer has placed an order: true or false
+
+		boolean order=true;
+		System.out.println(order);
+
+		//3.Person's phone number: "123-456-7890"
+		double MyContact=9603112606d;
+		System.out.println(MyContact);
+
+		// 4.Amount of money in a customer's bank account: 1000.50
+		float CBal=1000.50f;
+		System.out.println(CBal);
+
+		// 5.Person's email address: "john.doe@example.com"
+		String emailAdd="Gouthamkumar0706@gmail.com";
+		System.out.println(emailAdd);
+
+		// 6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitude=37.7749f;
+		double longitude=122.4194;
+		System.out.println(latitude);
+		System.out.println(longitude);
+
+		// 7.Person's marital status: true or false
+		boolean MaritalStatus=false;
+		System.out.println(MaritalStatus);
+
+		// 8.Person's occupation: "Software Engineer"
+		String Occupation="Software Engineer";
+		System.out.println(Occupation);
+		// 9.Person's favourite colour: "Blue"
+		String favColour="White";
+		System.out.println(favColour);
+
+		// 10.Current year: 2025
+		int CurrentYear=2025;
+		System.out.println(CurrentYear);
+
+		// 11.Number of followers on a social media platform: 1,000,000
+		int SMPfollowers=1000000;
+		System.out.println(SMPfollowers);
+
+		// 12.Rating of a movie: 7.5
+		double MovieRating=7.5;
+		System.out.println(MovieRating);
+
+		// 13.Person's blood type: 'A'
+		char BloodGroup='B';
+		System.out.println(BloodGroup);
+
+		// 14.Title of a book: "To Kill a Mockingbird"
+		String BookTitle="To Kill a Mockingbird";
+		System.out.println(BookTitle);
+
+		// 15.Number of employees in a company: 500
+		int TotalEmployees=500;
+		System.out.println(TotalEmployees);
+
+		// 16.Time of an event: 2:30 PM
+		String EvenTime="2:30 PM";
+		System.out.println(EvenTime);
+
+		// 17.Name of a country: "United States"
+		String Country="INDIA";
+		System.out.println(Country);
+
+		// 18.Person's eye color: "Brown"
+		String EyeColour="Brown";
+		System.out.println(EyeColour);
+
+		// 19.Person's birthplace: "New York City"
+		String BirthPlace="New York City";
+		System.out.println(BirthPlace);
+
+		// 20.Distance between two cities: 200.5
+		float Distance=200.5f;
+		System.out.println(Distance);
+
+
+	}
 }
+
+
